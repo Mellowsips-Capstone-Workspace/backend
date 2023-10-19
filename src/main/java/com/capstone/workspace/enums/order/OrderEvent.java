8b3c24727f66b6b7f5b0f81b7c5ea8bb257905de
unknown <felix@vnhcm-lc-226.contemi.com>
2023-10-20 00:31:19 +0700

    refactor

--- src/main/java/com/capstone/workspace/enums/order/OrderEvent.java
+++ src/main/java/com/capstone/workspace/enums/order/OrderEvent.java
@@ -3,11 +3,11 @@ package com.capstone.workspace.enums.order;
 public enum OrderEvent {
     TO_ORDERED,
     TO_PROCESSING,
+    TO_COMPLETED,
     CANCEL,
     REJECT,
     PROCESS,
     COMPLETE,
     RECEIVE,
-    DECLINE,
-    TO_COMPLETED
+    DECLINE
 }
